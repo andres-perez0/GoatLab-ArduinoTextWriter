2325fed852a895e01c44627935ff8183f7d6f2f1
Andres Perez <ap7552897@gmail.com>
2024-09-30 00:38:45 -0400

    added prototyped code

--- ArduinoTextWriter.py
+++ ArduinoTextWriter.py
@@ -3,30 +3,20 @@ import time
 
 # # serial.Serial('COMXX',baud rate)
 ser = serial.Serial(port='COM10',baudrate=115200)
-# ser.reset_input_buffer()
-# ser.flushInput()
+# Clears data that has already been received and waiting in the input buffer of a communication channel
+ser.reset_input_buffer()
 
-# code from (Python serial port communication using PySerial) https://www.youtube.com/watch?v=Kr1RyK6WENQ&
-while True:
-    try:
-        value=ser.readline()
-        valueInString=str(value,'UTF-8')
-        print(valueInString)
-    except KeyboardInterrupt:
-        print("stopped by user")
-        break
-
-
-# with open("arduino_output.txt", "w") as file:
-#     while True:
-#         try:
-#             # Read the data from Arduino
-#             if ser.in_waiting > 0:
-#                 arduino_output = ser.readline().decode('utf-8').strip()
-#                 print(arduino_output)  # Display the output
-#                 file.write(arduino_output + "\n")  # Save the output to the text file
-#                 file.flush()  # Ensure data is written to the file
-#                 time.sleep(1)  # Optional delay to match Arduino output
-#         except KeyboardInterrupt:
-#             print("Stopped by user")
-#             break
+# Creates and writes on ardunio_output.txt file
+with open("arduino_output.txt", "w") as file:
+    while True:
+        try:
+            # Read the data from Arduino
+            if ser.in_waiting > 0:
+                arduino_output = ser.readline().decode('utf-8').strip()
+                # print(arduino_output)  # Testing print statement to display the output
+                file.write(arduino_output + "\n")  # Save the output to the text file
+                file.flush()  # Ensure data is written to the file
+        # Hold ctrl + C to end the program
+        except KeyboardInterrupt:
+            print("Stopped by user")
+            break
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 pip install pyserial
 
 Make sure you're on the right python version
+
+# code from (Python serial port communication using PySerial) https://www.youtube.com/watch?v=Kr1RyK6WENQ&
